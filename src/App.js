8e4eba8164691001ad7ff82409618c81c87b34ed
AJ Kingston <akingstonlewis@gmail.com>
2022-03-13 15:53:29 -0700

    awd

--- src/App.js
+++ src/App.js
@@ -3,43 +3,43 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
-  const { darkMode } = useContext(DarkModeContext);
+	const { darkMode } = useContext(DarkModeContext);
 
-  return (
-    <div className={darkMode ? "app dark" : "app"}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
-              />
-            </Route>
-            <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
-              />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className={darkMode ? "app dark" : "app"}>
+			<HashRouter>
+				<Routes>
+					<Route path="/">
+						<Route index element={<Home />} />
+						<Route path="login" element={<Login />} />
+						<Route path="users">
+							<Route index element={<List />} />
+							<Route path=":userId" element={<Single />} />
+							<Route
+								path="new"
+								element={<New inputs={userInputs} title="Add New User" />}
+							/>
+						</Route>
+						<Route path="products">
+							<Route index element={<List />} />
+							<Route path=":productId" element={<Single />} />
+							<Route
+								path="new"
+								element={<New inputs={productInputs} title="Add New Product" />}
+							/>
+						</Route>
+					</Route>
+				</Routes>
+			</HashRouter>
+		</div>
+	);
 }
 
 export default App;
